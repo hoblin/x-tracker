b81504592e79459e5b9c64b7c8e7ec5dafd7b8dc
Yevhenii Hurin <evgeny.gurin@gmail.com>
2023-10-07 00:18:28 +0300

    Welcome page

---
+++ spec/requests/welcome_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe "Welcomes", type: :request do
+  describe "GET /index" do
+    subject { get "/welcome/index" }
+
+    it "returns http success" do
+      subject
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the index template" do
+      subject
+      expect(response).to render_template(:index)
+    end
+  end
+end
