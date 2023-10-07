da698e4d73454bf166ecfaffed0ff2a8994dc5cd
Yevhenii Hurin <evgeny.gurin@gmail.com>
2023-10-07 21:22:15 +0300

    Fix mobile navigation

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,18 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "chartkick/highcharts"
+
+// navbar burger
+document.addEventListener('turbo:load', () => {
+  const navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  if (navbarBurgers.length > 0) {
+    navbarBurgers.forEach(el => {
+      el.addEventListener('click', () => {
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+      });
+    });
+  }
+});
