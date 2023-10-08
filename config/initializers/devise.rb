e92a855514905e3db39d9903a18017f87b63f6f4
Yevhenii Hurin <evgeny.gurin@gmail.com>
2023-10-08 05:50:47 +0300

    And another one

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,7 +1,3 @@
-if Rails.autoloaders.zeitwerk_enabled?
-  Rails.autoloaders.main.ignore("#{Rails.root}/app/mailers")
-end
-
 Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
