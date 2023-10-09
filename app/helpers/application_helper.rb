2dae58f0548cc1c03c0cf97bd296996fccab0a85
Yevhenii Hurin <evgeny.gurin@gmail.com>
2023-10-09 15:14:51 +0300

    Add custom page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def title(title)
+    content_for(:title) { title }
+  end
+
+  def tweet_title(tweet)
+    title("Tracking Tweet by #{tweet.author_name}: #{tweet.body&.truncate(50)}")
+  end
 end
